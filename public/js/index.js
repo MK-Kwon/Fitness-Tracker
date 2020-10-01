ebea5cd4167959b76d6abb50882bb3c6041453d1
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-10-01 21:28:45 +0930

    Built index.js with function to get 'lastworkout' data and display it on the 'Last Workout' section

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+init();
+
+async function init() {
+    if(location.search.split("=")[1] === undefined) {
+        const workout = await API.getLastWorkout();
+        if(workout) {
+            location.search = "?id" + workout._id;
+        }
+        document.querySelector("#continue-btn").classList.add("d-none");
+        document.querySelector("#new-btn").style.display = "block";
+        document.querySelector("#new-btn").style.width = "100%";
+    }
+}
