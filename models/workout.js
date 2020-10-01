d046714eba091453ce8c869ed1a1d86f34cf7f5a
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-10-01 14:35:20 +0930

    Built models for schema

---
+++ models/workout.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: { type: Date, default: Date.now },
+    exercises: [
+        {
+            type: { type: String, required: true },
+            name: String,
+            duration: Number,
+            distance: Number,
+            weight: Number,
+            sets: Number
+        }
+    ],
+    totalDuration: Number
+});
+
+workoutSchema.methods.setTotalDuration = function() {
+    let total = 0;
+    for (let i = 0; i < this.exercises.length; i++) {
+        total += this.exercises[i].duration;
+    }
+    this.totalDuration = total;
+    return this.totalDuration;
+};
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
