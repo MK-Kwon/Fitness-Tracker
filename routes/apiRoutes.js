dd9af7156730d74a33f2dd7ad0f8ed4f84ce022a
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-10-01 17:31:59 +0930

    Built routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,62 @@
+const db = require("../models");
+
+module.exports = function(app) {
+    // "/api/workouts" GETS all workouts
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({})
+        .then(dbWorkouts => {
+            for (const workout of dbWorkouts) {
+                workout.setTotalDuration();
+            }
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+    // "/api/workouts/:id" UPDATES one workout's exercises by id
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.updateOne(
+            {
+                _id: req.params.id
+            },
+            {
+                $push: {
+                    exercises: req.body
+                }
+            }
+        )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+    // "/api/workouts" POSTS one workout
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create(req.body)
+        .then(dbWorkout => {
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+    // "/api/workouts/range" GET the 7 most recent workouts
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({})
+        .limit(7)
+        .sort({ day: -1 })
+        .then(dbWorkouts => {
+            for(const workout of dbWorkouts) {
+                workout.setTotalDuration();
+            }
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        })
+    })
+}
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = function(app) {
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(_dirname, "../public/index.html"));
+    });
+
+    app.get("/stats", (req, res) => {
+        res.sendFile(path.join(_dirname, "../public/stats.html"));
+    });
+
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(_dirname, "../public/exercise.html"));
+    });
+};
+
