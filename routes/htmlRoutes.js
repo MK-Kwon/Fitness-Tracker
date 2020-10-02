2f9eba989b36a3afb0fe4c947df280b23ce43a32
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-10-02 22:35:46 +0930

    Corrected typos

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,15 +2,15 @@ const path = require("path");
 
 module.exports = function(app) {
     app.get("/", (req, res) => {
-        res.sendFile(path.join(_dirname, "../public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
     app.get("/stats", (req, res) => {
-        res.sendFile(path.join(_dirname, "../public/stats.html"));
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
 
     app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(_dirname, "../public/exercise.html"));
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
 };
 
