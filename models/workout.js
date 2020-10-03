2ae3951d046be2ed6a1d3d499958c1b02d2c8c1b
Min Kyung Kwon <tolitoli_78@hotmail.com>
2020-10-03 13:00:50 +0930

    corrected error

--- models/workout.js
+++ models/workout.js
@@ -2,28 +2,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    day: { type: Date, default: Date.now },
-    exercises: [
-        {
-            type: { type: String, required: true },
-            name: String,
-            duration: Number,
-            distance: Number,
-            weight: Number,
-            reps: Number,
-            sets: Number
-        }
-    ],
-    totalDuration: Number
+  day: { type: Date, default: Date.now },
+  exercises: [
+    {
+      type: { type: String, required: true },
+      name: String,
+      duration: Number,
+      distance: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number
+    }
+  ],
+  totalDuration: Number
 });
 
 workoutSchema.methods.setTotalDuration = function() {
-    let total = 0;
-    for (let i = 0; i < this.exercises.length; i++) {
-        total += this.exercises[i].duration;
-    }
-    this.totalDuration = total;
-    return this.totalDuration;
+  let total = 0;
+  for (let i = 0; i < this.exercises.length; i++) {
+    total += this.exercises[i].duration;
+  }
+  this.totalDuration = total;
+  return this.totalDuration;
 };
 
 const Workout = mongoose.model("Workout", workoutSchema);
